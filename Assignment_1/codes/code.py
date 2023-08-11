6cd6df70388ed55e12d77c0ab423d8966e2cb55c
KillerOwen <gsingla402@gamil.com>
2023-08-11 19:15:34 +0530

    first commit

--- Assignment_1/codes/code.py
+++ Assignment_1/codes/code.py
@@ -30,26 +30,57 @@ import subprocess
 import shlex
 #end if
 
+A = np.array([[1,1],[5,-7]])             #Defining the vector A
+B = np.array([2,20])                     #Defining the vector B
+H = np.linalg.solve(A,B) 
+
 A = np.array([1,-1])
-D = np.array([-3.5,0.5])
+B = np.array([-4, 6])
+C = np.array([-3,-5])
+D = alt_foot(A,B,C)
+E = alt_foot(B,A,C)
+F = alt_foot(C,A,B)
+
+x_AB = line_gen(A,B)	
+x_BC = line_gen(B,C)
+x_CA = line_gen(C,A)
+x_AD = line_gen(A,D)
+x_AE = line_gen(A,E)
+x_BE = line_gen(B,E)
+x_CF = line_gen(C,F)
+x_AF = line_gen(A,F)
+x_CH = line_gen(C,H)
+x_BH = line_gen(B,H)
+x_AH = line_gen(A,H)
 
 #Triangle coordinates
 #[A,B,C] = tri_vert(a,b,c)
 
-#Generating all lines
-x_AB = line_gen(A,D)
-#x_BC = line_gen(B,C)
-#x_CA = line_gen(C,A)
 
 #Plotting all lines
-plt.plot(x_AB[0,:],x_AB[1,:],label='$AD$')
+plt.plot(x_AB[0,:],x_AB[1,:],label='$AB$')
+plt.plot(x_BC[0,:],x_BC[1,:],label='$BC$')
+plt.plot(x_CA[0,:],x_CA[1,:],label='$CA$')
+plt.plot(x_AD[0,:],x_AD[1,:],label='$AD_1$')
+plt.plot(x_BE[0,:],x_BE[1,:],label='$BE_1$')
+plt.plot(x_AE[0,:],x_AE[1,:],linestyle = 'dashed',label='$AE_1$')
+plt.plot(x_CF[0,:],x_CF[1,:],label='$CF_1$')
+plt.plot(x_AF[0,:],x_AF[1,:],linestyle = 'dashed',label='$AF_1$')
+plt.plot(x_CH[0,:],x_CH[1,:],label='$CH$')
+plt.plot(x_BH[0,:],x_BH[1,:],label='$BH$')
+plt.plot(x_AH[0,:],x_AH[1,:],linestyle = 'dashed',label='$AH$')
 
 #Labeling the coordinates
 A = A.reshape(-1,1)
+B = B.reshape(-1,1)
+C = C.reshape(-1,1)
 D = D.reshape(-1,1)
-tri_coords = np.block([[A,D]])
+E = E.reshape(-1,1)
+F = F.reshape(-1,1)
+H = H.reshape(-1,1)
+tri_coords = np.block([[A,B,C,D,E,F,H]])
 plt.scatter(tri_coords[0,:], tri_coords[1,:])
-vert_labels = ['A','D']
+vert_labels = ['A','B','C','D_1','E_1','F_1','H']
 for i, txt in enumerate(vert_labels):
     plt.annotate(txt, # this is the text
                  (tri_coords[0,i], tri_coords[1,i]), # this is the point to label
@@ -59,7 +90,7 @@ for i, txt in enumerate(vert_labels):
 
 plt.xlabel('$x$')
 plt.ylabel('$y$')
-plt.title('Plot of line AD')
+plt.title('Plot')
 plt.legend(loc='best')
 plt.grid() # minor
 plt.axis('equal')
@@ -67,20 +98,22 @@ plt.axis('equal')
 #code for calculations starts from here
 A = np.array([1,-1])
 print("Point A is : ",A)
-D = np.array([-3.5,0.5])
-print("Mid point of BC(D) is : ",D)
+B = np.array([-4,6])
+print("Point B is : ",B)
+C = np.array([-3,-5])
+print("Point C is : ",C)
+H = np.array([17/6,-5/6])
+print("Point H is : ",H)
 
-m = D-A
-print("m = ",m)
+val1 = A-H
+val2 = B-C
 
-nt = np.array([m[1]*1+m[0]*0,m[0]*-1+m[1]*0])
+result = val1@val2
 
-#calculation of constant we get on multiplying n^T and A
-nt = (nt*2)/3
-#the above step is done to cancel out the factor of 1/2 on both sides of equation
+#Result will come in the form of e-16, we can use round function to round it off to 0
+rounded = round(result,6)
 
-const = nt@A
-print("Value of n^T.A = ",const)
+print("The resultant of the product is : ",rounded)
 
 #calculation code ends here
 
